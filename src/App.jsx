26ad5c4b669190fc82fb6b7c743916c4b0791e53
afwerki <afexgh07@gmail.com>
2022-11-16 19:09:40 +0000

    new add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Amhara Developmnt Association</h1>
+        <h1>this is the second</h1>
+        <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./styles/style.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+         <ul>
+          <li>home</li>
+          <li>About us</li>
+          <li>our goal</li>
+          <li>donate</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
